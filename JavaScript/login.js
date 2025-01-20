f92394770ca2fb25677611e1171b419f39d35ddc
vidhii015 <vidhivekariya015@gmail.com>
2025-01-20 19:09:04 +0530

    update login.js

--- JavaScript/login.js
+++ JavaScript/login.js
@@ -14,13 +14,13 @@ const body = document.body;
 // Event listener to open the login card when the login link is clicked
 loginLink.addEventListener("click", () => {
   cardContainer.classList.remove("hidden"); // Show the login card
-  body.classList.add("overflow-hidden"); // Prevent body from scrolling
+  body.classList.add("overflow-y-hidden"); // Prevent body from scrolling
 
   const close = document.querySelector(".close");
 
   // Event listener to close the login card when the close button is clicked
   close.addEventListener("click", () => {
     cardContainer.classList.add("hidden"); // Hide the login card
-    body.classList.remove("overflow-hidden"); // Enable body scrolling
+    body.classList.remove("overflow-y-hidden"); // Enable body scrolling
   });
 });
